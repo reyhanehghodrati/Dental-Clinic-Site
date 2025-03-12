b6dc46fb19fe9ada74d844971b0d4ca949b9d8a7
Reyhaneh <reyhanghodrati@gmail.com>
2025-03-12 13:42:52 +0330

    جی اس کادر نوبت

--- php/save_request.php
+++ php/save_request.php
@@ -1,29 +1,20 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "dental_health";
-
-$conn = new mysqli($host, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("خطا در اتصال به پایگاه داده: " . $conn->connect_error);
-}
+include ('config.php');
 
 $fullName = $_POST['full_name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$appointmentTime = $_POST['appointment_time'];
+$nobat = $_POST['nobat'];
 
-if (empty($fullName) || empty($email) || empty($phone) || empty($appointmentTime)) {
+if (empty($fullName) || empty($email) || empty($phone) ||empty($nobat)) {
     $message = "لطفاً تمام فیلدها را پر کنید.";
     echo "<script type='text/javascript'>alert('$message');</script>";
 
     exit;
 }
 
-$stmt = $conn->prepare("INSERT INTO consultation_requests (full_name, email, phone, appointment_time) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("ssss", $fullName, $email, $phone, $appointmentTime);
+$stmt = $conn->prepare("INSERT INTO consultation_requests (full_name, email, phone, nobat) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $fullName, $email, $phone, $nobat);
 
 if ($stmt->execute()) {
     $message = "درخواست شما با موفقیت ثبت شد.";
